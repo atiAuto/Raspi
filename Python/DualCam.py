a502efa4d77a3c46d87448e4cef140e4ab9a7980
Jay Kim <sjkim2@ati2000.co.kr>
2018-08-02 10:38:14 +0900

    Change DualCam's display size 240 into 300

--- Python/DualCam.py
+++ Python/DualCam.py
@@ -1,5 +1,5 @@
 '''
-v1.2
+v1.3
 Display dual USB webcam
 Jay Kim
 
@@ -13,7 +13,7 @@ while True:
     ret1, frame1 = cap1.read()
     ret2, frame2 = cap2.read()
     cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-    cv2.resizeWindow('image', 640, 240)
+    cv2.resizeWindow('image', 640, 300)
 
     frame3 = np.hstack((frame1, frame2))
     cv2.imshow('image', frame3)
